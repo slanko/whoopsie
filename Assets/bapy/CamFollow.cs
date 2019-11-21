0006ce0c9af76fc7dc654c3bcd9bd83a6bcc46f2
slinky <QMBFXSL@gmail.com>
2019-11-22 11:45:35 +1300

    regtrthrthty
    jtyjtyjtyjtuk

---
+++ Assets/bapy/CamFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public GameObject followTarget;
+    public float lerpSpeed;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(new Vector3(0, -3, 0));
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(new Vector3(0, 3, 0));
+        }
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, followTarget.transform.position.x, lerpSpeed * Time.deltaTime), Mathf.Lerp(transform.position.y, followTarget.transform.position.y, lerpSpeed * Time.deltaTime), Mathf.Lerp(transform.position.z, followTarget.transform.position.z, lerpSpeed * Time.deltaTime));
+        //transform.rotation = new Quaternion(0, Mathf.Lerp(transform.rotation.y, followTarget.transform.rotation.y, lerpSpeed * Time.deltaTime), 0, 0);
+    }
+}
---
+++ Assets/bapy/heenut.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heenut : MonoBehaviour
+{
+    Animator anim;
+    public KeyCode up, down, left, right, attack, dodge, jump;
+    public float moveSpeed;
+    public bool canIMove;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(canIMove == true)
+        {
+            if (Input.GetKey(left))
+            {
+                transform.rotation = new Quaternion(0, -180, 0, 0);
+                anim.SetBool("running", true);
+                transform.Translate(new Vector3(1, 0, 0) * moveSpeed);
+            }
+            if (Input.GetKey(right))
+            {
+                transform.rotation = new Quaternion(0, 0, 0, 0);
+                anim.SetBool("running", true);
+                transform.Translate(new Vector3(1, 0, 0) * moveSpeed);
+            }
+            if (Input.GetKey(left) == false && Input.GetKey(right) == false)
+            {
+                anim.SetBool("running", false);
+            }
+            if (Input.GetKey(left) && Input.GetKey(right))
+            {
+                anim.SetBool("running", false);
+            }
+            if (Input.GetKeyDown(dodge))
+            {
+                anim.SetTrigger("dodge");
+            }
+            if (Input.GetKeyDown(attack))
+            {
+                anim.SetTrigger("smash");
+            }
+            if (Input.GetKeyDown(jump))
+            {
+
+            }
+        }
+    }
+
+    public void canMove()
+    {
+        canIMove = true;
+    }
+
+    public void cantMove()
+    {
+        canIMove = false;
+    }
+}
