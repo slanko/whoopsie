226110771b1189298bd9d56774ac205764b1c3ad
slinky <QMBFXSL@gmail.com>
2019-12-10 11:48:03 +1300

    e
    a

---
+++ Assets/efef.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class efef : MonoBehaviour
+{
+    NavMeshAgent nav;
+    Vector3 navPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        changePosition();
+    }
+
+    void changePosition()
+    {
+        Invoke("changePosition", Random.Range(0.1f, 1f));
+        navPos = Random.insideUnitSphere * Random.Range(5, 30) + transform.position;
+        nav.SetDestination(navPos);
+    }
+}
